7481d4e459592db94d36ec34e31d746a13661f12
Evgeniy Budaev <budaev.e@gmail.com>
2022-06-06 08:59:10 +0300

    Объявляем переменные

---
+++ src/level00/lesson04/Task3.java
@@ -0,0 +1,17 @@
+package level00.lesson04;
+
+/*
+Объявляем переменные
+*/
+
+/*Напиши программу, которая в методе main объявляет такие переменные:
+name типа String, age типа int и city типа String.*/
+
+public class Task3 {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        String name;
+        int age;
+        String city;
+    }
+}
