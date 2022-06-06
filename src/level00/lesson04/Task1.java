248486f19398cd3767df81c1f493f8828511c08a
Evgeniy Budaev <budaev.e@gmail.com>
2022-06-06 08:53:21 +0300

    Комплимент учителю

---
+++ src/level00/lesson04/Task1.java
@@ -0,0 +1,17 @@
+package level00.lesson04;
+
+/*
+Комплимент учителю
+*/
+
+/*Написать программу, выводящую на экран надпись "Элли самая красивая" 5 раз. Каждый раз - с новой строки.*/
+public class Task1 {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        System.out.println("Элли самая красивая");
+        System.out.println("Элли самая красивая");
+        System.out.println("Элли самая красивая");
+        System.out.println("Элли самая красивая");
+        System.out.println("Элли самая красивая");
+    }
+}
