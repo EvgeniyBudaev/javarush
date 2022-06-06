9d0a102287f34a045510732cb324259ac5970b6c
Evgeniy Budaev <budaev.e@gmail.com>
2022-06-06 08:56:47 +0300

    Мой юный друг

---
+++ src/level00/lesson04/Task2.java
@@ -0,0 +1,16 @@
+package level00.lesson04;
+
+/*
+Мой юный друг
+*/
+
+/*Сейчас 3126 год. Мой друг родился 8 лет назад.
+Написать программу, выводящую на экран год рождения моего друга.*/
+
+public class Task2 {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        int year = 3126 - 8;
+        System.out.println(year);
+    }
+}
