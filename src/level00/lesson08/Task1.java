f8ceb6e18e29347e4ee59e3a9d4da0138e46ee7f
Evgeniy Budaev <budaev.e@gmail.com>
2022-06-06 09:22:35 +0300

    Баги и фичи

---
+++ src/level00/lesson08/Task1.java
@@ -0,0 +1,14 @@
+package level00.lesson08;
+
+/*
+Баги и фичи
+*/
+
+/*Напиши программу, которая выводит на экран надпись: Это не баг, это фича.*/
+
+public class Task1 {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        System.out.println("Это не баг, это фича.");
+    }
+}
